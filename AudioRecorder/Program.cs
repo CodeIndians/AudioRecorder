354c59f6edab7c1e621fd8f6587134334854afb9
Raghuveer Ch S <raghuveer@codeindians.com>
2024-01-06 12:45:53 +0530

    Added winMMBase and recorder classes

--- AudioRecorder/Program.cs
+++ AudioRecorder/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using AudioRecorder;
+
+void appFunction()
+{
+    var record = new Record();
+    record.
+}
+
+
+appFunction();
